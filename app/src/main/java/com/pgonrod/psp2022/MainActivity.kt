ebdadc1de40248b1a30e921daf41aa20a1fcda74
Pabloyquemas <114018509+Pabloyquemas@users.noreply.github.com>
2022-10-05 19:03:57 +0200

    Retrofitv2

--- app/src/main/java/com/pgonrod/psp2022/MainActivity.kt
+++ app/src/main/java/com/pgonrod/psp2022/MainActivity.kt
@@ -2,6 +2,7 @@ package com.pgonrod.psp2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.pgonrod.psp2022.ut02.data.remote.RetrofitApiClient
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Thread {
+            val users =  apiClient.getUsers()
+            Log.d("@dev", "Usuarios: $users")
+        }.start()
 
-        apiClient.getUsers()
     }
 }
